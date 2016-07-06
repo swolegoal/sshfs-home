d9679922a48c4184e449f4035f285a4b92f13297
Andrew M. Rogers <tuxlovesyou@gmail.com>
2016-07-06 16:52:51 -0400

    Prompt for server IP address.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+#function create_key {
+
+#}
 
 if [ "$(whoami)" != "root" ] ; then
         echo -e "This command can only be run as root\n"
 	exit
 fi
 
-
-
+read -p 'Enter the IP address of the remote server: ' ip
+sed -i '5s/.*/remote_ip=\"'$ip'\"/' sshfs-home
